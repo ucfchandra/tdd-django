d56f4e8f09102e585a7c24e0e750e6e0309baafa
ucfchandra <chandra.kethireddy@ucf.edu>
2024-06-22 04:30:54 -0400

    url config, map / to home_page view

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,10 +3,9 @@ from django.http import HttpRequest
 from lists.views import home_page
 
 class HomePageTest(TestCase):
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode("utf-8")
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.startswith("<html>"))
-        self.assertTrue(html.endswith("</html>"))
+    def test_home_page_returns_correct_html(self): 
+        response = self.client.get("/") 
+        self.assertContains(response, "<title>To-Do lists</title>")
+        self.assertContains(response, "<html>")
+        self.assertContains(response, "</html>")
+
