83fad22f07410c269f09a9bd763bcc0fb18a1691
ucfchandra <chandra.kethireddy@ucf.edu>
2024-06-22 04:03:40 -0400

    First FT specced out in commends, and now uses unittest

---
+++ superlists/functional_tests.py
@@ -0,0 +1,36 @@
+import unittest
+from selenium import webdriver
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_todo_list(self):
+        # Ima has heard about a cool new online to-do app
+        # She goes to check out its homepage
+        self.browser.get("http://0.0.0.0:8000")
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn("To-Do", self.browser.title)
+
+        # She is invited to enter a to-do item straight away
+        self.fail("Finish the test!")
+
+# She types "Buy peacock feathers" into a text box
+# (Ima's hobby is tying fly-fishing lures)
+
+# When she hits enter, the page updates, and now the page lists 
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting her to add another item.
+# She enters "Use peacock feathers to make a fly" (Ima is very methodical)
+
+# The page updates again, and now shows both items on her list
+
+# Satisfied, she goes back to sleep
+
+if __name__ == "__main__":
+    unittest.main()
